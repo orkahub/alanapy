89f417db1369e54a801b4d893e9823d24d68e2ac
LuisNavarro <93lenm@gmail.com>
2023-09-10 21:40:26 -0600

    Update version .23 with setup tools 63.4.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="alanapy",
-    version="0.20",
+    version="0.23",
     license="MIT",
     author = "Orkahub Energy",
 	author_email = "orkahub@gmail.com",
